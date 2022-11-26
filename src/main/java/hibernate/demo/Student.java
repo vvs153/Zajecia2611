ccdf623cb4982e14a4f3f09b8aee194dd2d63d74
Kuba Postol <vvs153@gmail.com>
2022-11-26 13:27:03 +0100

    builder

--- src/main/java/hibernate/demo/Student.java
+++ src/main/java/hibernate/demo/Student.java
@@ -2,13 +2,21 @@ package hibernate.demo;
 
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import java.time.LocalDate;
+//wzorzec projektowy builder:
+//Kiedy uzywac:
+// - kiedy chcemy miec wieksza kontrole nad tworzeniem obiektow - mamy  na mysli ilosc i kolejnosc parametrow
+// - kiedy chcemy zredukowac ilosc konstruktorow (konstruktory o roznych kombinacjach parametrow;
 
 @Data
 @Entity
+@Builder
+@AllArgsConstructor
 @NoArgsConstructor
 public class Student {
     @Id // PRIMARY KEY
@@ -20,6 +28,8 @@ public class Student {
     private LocalDate dataUrodzenia;                                        // `data_urodzenia`         DATE,
     private String kierunekNauczania;                                       // `kierunek_nauczania`     VARCHAR(255),
     private String indeks; // index jest słowem zabronionym przez mysql     // `indeks`                 VARCHAR(255)
+
+    //zamiast pisac kontrkutory robimy builder
 
 //    private int wiek;                                                       // `wiek`                   INT NOT NULL,
 //    private Integer wiek;                                                   // `wiek`                   INT,
