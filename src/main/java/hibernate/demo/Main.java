c0520e3848c9a2d1bbf971a36d57e2a7349610f1
Kuba Postol <vvs153@gmail.com>
2022-11-26 12:42:59 +0100

    dodanie nowej encji

--- src/main/java/hibernate/demo/Main.java
+++ src/main/java/hibernate/demo/Main.java
@@ -1,10 +1,29 @@
 package hibernate.demo;
 
 import org.hibernate.Session;
+import org.hibernate.Transaction;
+
+import java.io.IOException;
+import java.time.LocalDate;
 
 public class Main {
     public static void main(String[] args) {
-       Session session =  HibernateUtil.INSTANCE.getSessionFactory().openSession();
-       session.close();
+        // wywołaj try-with-resources który zamknie sesję automatycznie po opuszczeniu try
+        try (Session session = HibernateUtil.INSTANCE.getSessionFactory().openSession()){
+       Transaction transaction = session.beginTransaction();
+       //tworzymy studenta
+       Student student = new Student();
+       student.setImie("pawel");
+       student.setDataUrodzenia(LocalDate.of(1990,1,3));
+       student.setKierunekNauczania("infromatyka");
+       student.setIndeks("124354");
+       //zapisujemy studenta
+       session.persist(student);
+       //potwierdzamy trasakcje
+            transaction.commit();
+        } catch (Exception ioe) {
+            // jeśli złapiemy błąd, to wywoła się catch
+
+        }
     }
 }
